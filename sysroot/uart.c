a1774295978c522ddd89f7547483da6b5fbecbe9
JussiKarkkainen <juskark@gmail.com>
2022-01-13 21:30:19 +0200

    read/write + console start

--- sysroot/uart.c
+++ sysroot/uart.c
@@ -4,6 +4,8 @@
 #define LCR_8BIT 0x3
 #define FIFO_ENABLE 0x1
 #define IER_ENABLE 0xf
+#define ERROR_BITMASK 
+
 
 typedef volatile struct __attribute__((packed)) {   // Tell compiler to not padd struct
     uint32_t BF;            // Read/Write registers
@@ -19,7 +21,6 @@ typedef volatile struct __attribute__((packed)) {   // Tell compiler to not padd
 
 
 static volatile uart_regs* uart = (uart_regs*)0x10000000;
-
 
 
 typedef enum {     
@@ -64,6 +65,48 @@ uart_return uart_configure(uart_init* init) {
 }
   
 
+void putchar(char c) {
+    uart->BF = c;
+}
+
+void write_uart(char* data) {
+    while (*data) {
+        uart_putchar(*data);
+        *data++;
+    }
+}
+
+
+uart_return read_uart(char* c) {
+    
+    *c = uart->bf;
+    if (uart->LSR & ERROR_BITMASK) {    // Check for errors
+        uart->LSR &= ERROR_BITMASK;     // Reset error register
+        return UART_FAIL;
+    } 
+
+
+    return UART_OK;
+}
+
+
+int main() {
+    
+    uart_init init = {
+        .word_length = 8,
+        .FIFO = 1,
+        .interrupt_enable = 1,
+        .baudrate = 9600            // This isn't used
+    };
+
+    uart_init(&init);
+
+    uart_putchar("A");
+    uart_putchar("\n");
+    uart_write("Hello World\n");
+
+    return 0;
+}
 
 
 
