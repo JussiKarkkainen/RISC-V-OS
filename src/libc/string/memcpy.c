31e66b1510253769168b2422d1d6c80fade93789
JussiKarkkainen <juskark@gmail.com>
2022-01-22 21:06:09 +0200

    add memcpy

---
+++ src/libc/string/memcpy.c
@@ -0,0 +1,9 @@
+#include <string.h>
+
+void *memcpy(void *s1, const void *s2, size_t n) {
+    unsigned char *dst = (unsigned char *)s1;
+    unsigned char *src = (const unsigned char *)s2;
+    for (int i=0; i<=n; i++) 
+        dst[i] = src[i];
+    return s1;
+}
