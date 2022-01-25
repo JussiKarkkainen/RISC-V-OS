0123573e69230e97bc4f608956d79cc40b109cc5
JussiKarkkainen <juskark@gmail.com>
2022-01-25 19:36:09 +0200

    write init function

--- src/kernel/arch/riscv/kalloc.c
+++ src/kernel/arch/riscv/kalloc.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
-
+// Need to have one page_info for each page
 typedef struct {
     uint8_t flags;
-} page;
+} page_info;
 
 
 typedef enum {
@@ -13,7 +13,30 @@ typedef enum {
 } page_bits;
 
 
+int align(int value, int align) {
+    int c = (1 << align) - 1;
+    return value + (~value & c);   
+}
+
+int align = 12;
+int ALLOC_START;
+int page_size = 1 << 12;
+
+// Initialize descriptors
 void init() {
+    int num_pages = heap_size / page_size; 
+    
+    // Clear pages
+    int *start_addr = HEAP_START;
+    for (int i=0; i<= num_pages; i++) {
+        *start_addr = 0;
+        start_addr++;
+    }
+
+    // Determine location of useful memory
+    ALLOC_START = align(HEAP_START + (num_pages * page_size,
+                        align);
+
 }
 
 
