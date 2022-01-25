66f2759abfb7fa48da8f06c6fb177be4274c70de
JussiKarkkainen <juskark@gmail.com>
2022-01-25 21:47:25 +0200

    check for contiguos blocks

--- src/kernel/arch/riscv/kalloc.c
+++ src/kernel/arch/riscv/kalloc.c
@@ -22,13 +22,16 @@ int ALLOC_START;
 int page_size = 1 << 12;
 int num_pages = heap_size / page_size; 
 
+void clear(*addr) {
+    *addr = page_bits->EMPTY;
+}
+
 void init() {
 
     // Clear pages
     int *start_addr = HEAP_START;
     for (int i=0; i<= num_pages; i++) {
-        *start_addr = 0;
-        start_addr++;
+        clear(start_addr + i);
     }
 
     // Determine location of useful memory
@@ -36,16 +39,17 @@ void init() {
                         align);
 }
 
-void clear(addr) {
-    *addr = page_bits->EMPTY;
+bool is_free(*flags) {
+    if (*flags & (page_bits->TAKEN) != 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
-bool is_taken(flags) {
-    if (flags & (page_bits->LAST) != 0) {
-
-}
-
-void set_flags() {
+void set_flags(*page_descriptor) {
+    *page_descriptor = page_bits->TAKEN
 }
 
 void kalloc(size_t num_pages) {
@@ -53,10 +57,18 @@ void kalloc(size_t num_pages) {
     for (int i=0; 1<=num_pages; i++) {
         int found = 0;
         
-        if (is_free(*ptr) == 1) {
+        if (is_free(*start_addr) == 1) {
             found = 1;
+            
+            for (int j=0; j<=num_pages; j++) {
+                if (is_free(start_addr + j) == 0) {
+                    found = 0;
+                    break;
+                }
+            }
 
-
+        }
+    }
 }
 
 
