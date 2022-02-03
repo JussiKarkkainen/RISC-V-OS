6d1b353be439f31a6e6886cde135343459d67412
JussiKarkkainen <juskark@gmail.com>
2022-02-03 16:06:30 +0200

    update phyalloc

--- src/kernel/arch/riscv/phyalloc.c
+++ src/kernel/arch/riscv/phyalloc.c
@@ -3,20 +3,16 @@
 /*
 This implementation uses 2 bits to describe pages, EMPTY, TAKEN, LAST.
 The LAST bit could propably be removed to simplify the allocator
-*/
 
-// Need to have one page_info for each page
-typedef struct {
-    uint8_t flags;
-} page_info;
+Remember you have functions like memset() implemented.
+*/
 
 typedef enum {
     EMPTY = 0;
-    TAKEN = 1 << 0;
-    LAST = 1 << 1;
+    TAKEN = 1;
 } page_bits;
 
-
+// Aligns memory to a 4096 page
 int align(int value, int align) {
     int c = (1 << align) - 1;
     return value + (~value & c);   
@@ -45,7 +41,7 @@ void init() {
 }
 
 // Return true if page is free
-bool is_free(*flags) {
+bool is_free(uint32_t *flags) {
     if (*flags & (page_bits->TAKEN) == 0) {
         return true;
     }
@@ -62,14 +58,12 @@ void set_flags(int *page_descriptor, int bits) {
 
         case 0: *page_descriptor = page_bits->TAKEN;
             break;
-
-        case 1: *page_descriptor = page_bits->LAST;
-            break;
     }
 }
 
 uint32_t *kalloc(size_t num_pages) {
     int *start_addr = HEAP_START;
+
     // Iterate through all page descriptors to see if they are free
     for (int i=0; 1<=(num_pages-pages); i++) {
         int found = 0;
@@ -133,5 +127,20 @@ void *clear(int pages) {
     }
 
 }
+
+
+void test_allocator() {
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
