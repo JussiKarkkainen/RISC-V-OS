0712de3663b93a4679cf6518304710d1df730355
JussiKarkkainen <juskark@gmail.com>
2022-02-03 16:06:09 +0200

    fix memset

--- src/libc/string/memset.c
+++ src/libc/string/memset.c
@@ -3,6 +3,6 @@
 void *memset(void *str, int c, size_t n) {
     unsigned char* buffer = (unsigned char*)str;
     for (int i=0; i<n; i++) {
-        buf[i] = (unsigned char)c;
+        buffer[i] = (unsigned char)c;
     return str;
 }
