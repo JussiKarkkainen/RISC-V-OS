1fe512315b84f80072e7e3c18eae712cebec3b02
JussiKarkkainen <juskark@gmail.com>
2022-02-10 19:51:56 +0200

    get vpn

--- src/kernel/paging.c
+++ src/kernel/paging.c
@@ -2,4 +2,27 @@
 
 
 
+// Mapping a virtual address to a physical address
+
+int map(uint32_t *root, uint32_t viraddr, uint32_t, phyaddr, int bits, int level){
+    // Check if READ, WRITE, and EXECUTE bits are set
+
+    if (bits & 0xe != 0) {
+
+        // Get the virtual page number
+        // VPN[0] = bits 21-12, VPN[1] = bits 31-22.
+        uint32_t vpn = (((viraddr >> 22) & 0x3ff) << 10) | ((viraddr >> 12) & 0x3ff);
+
+
+
+
+}
+
+
+
+int unmap() {
+}
+
+int translate_to_phy() {
+}
 
