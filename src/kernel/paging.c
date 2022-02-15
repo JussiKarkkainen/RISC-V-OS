24b03f36f927964f595d39a31b6e82a3f9296f02
JussiKarkkainen <juskark@gmail.com>
2022-02-15 19:24:36 +0200

    rewrite is_valid()

--- src/kernel/paging.c
+++ src/kernel/paging.c
@@ -56,40 +56,6 @@ static inline void satp_write(uint32_t kpage) {
 }
 
 
-// Mapping a virtual address to a physical address
-
-int kmap(uint32_t *root, uint32_t viraddr, uint32_t, phyaddr, int bits, int level){
-    
-    // Check if READ, WRITE, and EXECUTE bits are set
-    if (bits & 0xe != 0) {
-
-        // Get the virtual page number
-        // VPN[0] = bits 21-12, VPN[1] = bits 31-22.
-        uint32_t vpn[20] = ((viraddr >> 22) & 0x3ff),  ((viraddr >> 12) & 0x3ff);
-
-        // Get physical page number
-        // Otherwise same except PPN[1] is 12 bits instead of 10
-        long long ppn[22] = {((viraddr >> 22) & 0xfff), ((viraddr >> 12) & 0xfff)};
-        
-        uint32_t v = *root[vpn[2]];
-
-        for (int i = 2; i > 0; i--) {
-            if (is_valid(v) == 0) {
-                
-                uint32_t page = zalloc(1);
-                
-
-
-            }
-        }   
-    }   
-}
-
-
-
-int kunmap() {
-}
-
 // Check if valid bit is set in page table entry
 bool is_valid(uint32_t pte) {
     if (pte & PTE_V)
@@ -101,21 +67,51 @@ bool is_valid(uint32_t pte) {
 // Check if page table entry is a leaf
 bool is_leaf(uint32_t pte) {
     // pte is leaf if any of the rx bits are set
-    if (pte & PTE_RX) 
-        return true;
-    else
-        return false;
+    return (pte & PTE_RX); 
 }
+
+// Mapping a virtual address to a physical address
+// root = address to kernel pagetable
+int kmap(uint32_t *root, uint32_t viraddr, uint32_t, phyaddr, int bits, int level){
+    
+    // Check if READ, WRITE, and EXECUTE bits are set
+    if (bits & 0xe != 0) {
+
+        // Get the virtual page number
+        // VPN[0] = bits 21-12, VPN[1] = bits 31-22.
+        uint32_t vpn[2] = {((viraddr >> 22) & 0x3ff),  ((viraddr >> 12) & 0x3ff)};
+
+        // Get physical page number
+        // Otherwise same except PPN[1] is 12 bits instead of 10
+        long long ppn[2] = {((viraddr >> 22) & 0xfff), ((viraddr >> 12) & 0xfff)};
+        
+        uint32_t v = *root[vpn[2]];
+
+        for (int i = 2; i > 0; i--) {
+            if (is_valid(v) == 0) {
+                
+                uint32_t page = zalloc(1);
+                 
+
+            }
+        }   
+    }   
+}
+
+
+int kunmap() {
+}
+
 
 int translate_to_phy(uint32_t *root, uint32_t viraddr) {
 
     // Get the virtual page number
     // VPN[0] = bits 21-12, VPN[1] = bits 31-22.
-    uint32_t vpn[20] = ((viraddr >> 22) & 0x3ff),  ((viraddr >> 12) & 0x3ff);
+    uint32_t vpn[2] = {((viraddr >> 22) & 0x3ff),  ((viraddr >> 12) & 0x3ff)};
 
     // Get physical page number
     // Otherwise same except PPN[1] is 12 bits instead of 10
-    long long ppn[22] = {((viraddr >> 22) & 0xfff), ((viraddr >> 12) & 0xfff)};
+    long long ppn[2] = {((viraddr >> 22) & 0xfff), ((viraddr >> 12) & 0xfff)};
     
 
     uint32_t *v = *root[vpn[2]];
