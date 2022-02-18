c5dd5e78ad0dc310814fdc0c91cb682f9879f51f
JussiKarkkainen <juskark@gmail.com>
2022-02-18 21:31:58 +0200

    define boot sequence

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,17 +2,18 @@
 #include "arch/riscv/uart.h"
 #include "../libc/include/stdio.h"
 #include "../libc/stdio/kprintf.c"
-#include "pmm.c"
+#include "arch/riscv/mem.S"
+//#include "pmm.c"
 //#include "paging.h"
 //#include "paging.c"
 
 void enter(void) {
     kprintf("%s\n", "Booting OS");
-    
-    pmm_init();
 
-    //kpage_init();
-    //init_paging();
-    console_init();
+    //pmm_init();         // Initialize physical memory manager
+    kpage_init();       // Initilaize kernel pagetable
+    init_paging();      // Initialize paging
+    
+    console_init();     // Start console
 
 }
