c4b62f34ed4b44036c6ab109c615838c4ff61835
JussiKarkkainen <juskark@gmail.com>
2022-02-18 21:43:23 +0200

    update kernel boot sequence

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,15 +2,14 @@
 #include "arch/riscv/uart.h"
 #include "../libc/include/stdio.h"
 #include "../libc/stdio/kprintf.c"
-#include "arch/riscv/mem.S"
-//#include "pmm.c"
-//#include "paging.h"
-//#include "paging.c"
+#include "pmm.c"
+#include "paging.h"
+#include "paging.c"
 
 void enter(void) {
     kprintf("%s\n", "Booting OS");
 
-    //pmm_init();         // Initialize physical memory manager
+    pmm_init();         // Initialize physical memory manager
     kpage_init();       // Initilaize kernel pagetable
     init_paging();      // Initialize paging
     
