ebc8734541ac8cbfb17e55db1f85ac6a5ee93aac
JussiKarkkainen <juskark@gmail.com>
2022-02-20 19:32:40 +0200

    start trap handlers

---
+++ src/kernel/trap.c
@@ -0,0 +1,12 @@
+// Machine mode trap handler
+
+uint32_t machine_trap(uint32_t mepc, uint32_t mtval, uint32_t mcause, 
+                      uint32_t hart, uint32_t mstatus) {
+
+// Check if trap is asynchronous or synchronous
+
+is_async = 
+
+
+}
+
