f9d0873e241a7b20eaa8580764f9310d0a1a7fc9
JussiKarkkainen <juskark@gmail.com>
2022-02-21 20:34:05 +0200

    prototype trap handling

--- src/kernel/trap.c
+++ src/kernel/trap.c
@@ -1,12 +1,26 @@
 // Machine mode trap handler
 
-uint32_t machine_trap(uint32_t mepc, uint32_t mtval, uint32_t mcause, 
-                      uint32_t hart, uint32_t mstatus) {
+void ktrap(void) {
 
-// Check if trap is asynchronous or synchronous
+    uint32_t sepc = get_sepc;
+    uint32_t sstatus = get_sstatus();
+    uint32_t scause = get_scause();
 
-is_async = 
+    // Make sure interrupt comes from supervisor mode
+    if ((sstatus & SSTATUS_SPP) == 0) {
+        kprintf("trap not in supervisor mode");
+    }
+    // Make sure interrupts are not enabled
+    if (check_interrupts() == 0) {
+        kprintf("interrupts are enabled");
+    }
+    if (handle_interrupt() == 0) {
+        kprintf("kernel interrupt");
+    }
+        
 
-
+    if (timer_interrupt == 1) {
+        // deal with timer interrupt
+    }
 }
 
