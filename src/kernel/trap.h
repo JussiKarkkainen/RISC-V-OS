e76351598520036802a875d65957a272bd17828f
JussiKarkkainen <juskark@gmail.com>
2022-02-24 20:58:48 +0200

    add trapframe struct

--- src/kernel/trap.h
+++ src/kernel/trap.h
@@ -1,12 +1,50 @@
 #ifndef TRAP_H
 #define TRAP_H
 
-
 #define SSTATUS_SPP (1 << 8)
 #define SSTATUS_SIE (1 << 1)
 #define INTERRUPT_BIT (1 << 32)
 #define EXT_INTERRUPT 0xff
 #define SOFT_INTERRUPT 0x80000001
+
+struct trapframe {
+    uint32_t kernel_pagetable;
+    uint32_t kernel_sp;
+    uint32_t kernel_trap;
+    uint32_t saved_pc;
+    uint32_t hartid;
+    uint32_t ra;
+    uint32_t sp;
+    uint32_t gp;
+    uint32_t tp;
+    uint32_t t0;
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t s0;
+    uint32_t s1;
+    uint32_t a0;
+    uint32_t a1;
+    uint32_t a2;
+    uint32_t a3;
+    uint32_t a4;
+    uint32_t a5;
+    uint32_t a6;
+    uint32_t a7;
+    uint32_t s2;
+    uint32_t s3;
+    uint32_t s4;
+    uint32_t s5;
+    uint32_t s6;
+    uint32_t s7;
+    uint32_t s8;
+    uint32_t s9;
+    uint32_t s10;
+    uint32_t s11;
+    uint32_t t3;
+    uint32_t t4;
+    uint32_t t5;
+    uint32_t t6;
+};
 
 static inline uint32_t get_mstatus(void) {
     uint32_t mstatus;
@@ -89,7 +127,6 @@ static inline void write_mscratch(uint32_t x) {
 static inline void write_mtvec(uint32_t x) {
     asm volatile("csrw mtvec, %0" : : "r" (x));
 }
-
 
 static inline uint32_t get_mie(void) {
     uint32_t mie;
