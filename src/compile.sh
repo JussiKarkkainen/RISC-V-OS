e59cf1c679764044dd05ae96955eaa63ba8929db
JussiKarkkainen <juskark@gmail.com>
2022-02-27 10:57:51 +0200

    update compile script

--- src/compile.sh
+++ src/compile.sh
@@ -1,2 +1,3 @@
 riscv64-unknown-elf-gcc -Wall -Wextra -march=rv32im -mabi=ilp32 -T kernel/linker.ld -nostdlib -ffreestanding -lgcc \
-kernel/boot.S kernel/kernel.c kernel/console.c kernel/uart.c libc/stdio/kprintf.c kernel/paging.c 
+kernel/boot.S kernel/kernel.c kernel/console.c kernel/uart.c libc/stdio/kprintf.c kernel/pmm.c kernel/mem.S \
+kernel/paging.c libc/string/memset.c
