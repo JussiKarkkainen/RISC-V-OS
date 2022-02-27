fe8a4d3647dcd02102d55e5a2c42337c924b26ee
JussiKarkkainen <juskark@gmail.com>
2022-02-27 21:40:20 +0200

    finish plic

--- src/kernel/plic.c
+++ src/kernel/plic.c
@@ -2,20 +2,28 @@
 
 #define PLIC 0xc000000
 #define PLIC_CLAIM_OFFSET 0x201004
-#define PLIC_ENABLE_OFFSET
+#define PLIC_ENABLE_OFFSET 0x2000
+#define PLIC_THRESHOLD 0x2080
+#define UART_IRQ 10
+#define VIRTIO_IRQ 1
 
 // https://github.com/qemu/qemu/blob/master/include/hw/riscv/virt.h
-// Uarts intr_id = 10. 
+// UART intr_id = 10.
+// VIRTIO intr_id = 1-8
+// PCIE intr_id = 32-35
+
 
 void plic_init(void) {
-    
+    (PLIC + UART_IRQ * 4) = 1;
+    (PLIC + VIRTIO_IRQ * 4) = 1;
 }
 
 void plic_init_hart(void) {
     int hart_id = which_cpu();
-
     // Set enable bit for this hart
-    (PLIC + PLIC_ENABLE_OFFSET + hart_id * 0x100) = 
+    (PLIC + PLIC_ENABLE_OFFSET + hart_id * 0x100) = (1 << UART_IRQ) | (1 << VIRTIO_IRQ);
+    // Set priority threshold 
+    (PLIC + PLIC_THRESHOLD + hart_id * 0x100) = 0;
 }
 
 int plic_read(void) {
