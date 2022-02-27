0cdb51c0b829c0d9e91be929486e0fa68ab1d5f8
JussiKarkkainen <juskark@gmail.com>
2022-02-27 13:49:32 +0200

    add plic_finished()

--- src/kernel/plic.c
+++ src/kernel/plic.c
@@ -2,13 +2,21 @@
 #define PLIC_CLAIM_OFFSET 0x201004
 
 void plic_init(void) {
+    
+}
+
+void plic_init_hart(void) {
+    
 }
 
 int plic_read(void) {
-    hart_id = which_cpu();
+    int hart_id = which_cpu();
+    // get interrupt ids from plic
     intr_id = (PLIC + PLIC_CLAIM_OFFSET + hart_id * 0x2000);
     return intr_id;
 }
 
-int plic_finished(int intr_id) {
+void plic_finished(int intr_id) {
+    int hart_id = which_cpu();
+    (PLIC + PLIC_CLAIM_OFFSET + hart_id * 0x2000) = intr_id;
 }
