35d0ece52ff431069c6440ed661856a1425d9dcd
JussiKarkkainen <juskark@gmail.com>
2022-02-27 14:02:49 +0200

    add plic_init_hart

--- src/kernel/plic.c
+++ src/kernel/plic.c
@@ -1,12 +1,21 @@
+#include "process.h"
+
 #define PLIC 0xc000000
 #define PLIC_CLAIM_OFFSET 0x201004
+#define PLIC_ENABLE_OFFSET
+
+// https://github.com/qemu/qemu/blob/master/include/hw/riscv/virt.h
+// Uarts intr_id = 10. 
 
 void plic_init(void) {
     
 }
 
 void plic_init_hart(void) {
-    
+    int hart_id = which_cpu();
+
+    // Set enable bit for this hart
+    (PLIC + PLIC_ENABLE_OFFSET + hart_id * 0x100) = 
 }
 
 int plic_read(void) {
