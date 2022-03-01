66f5efb1a71013e907d5b45613c449110c7def9d
JussiKarkkainen <juskark@gmail.com>
2022-03-01 19:35:58 +0200

    add lock interrupt disable function

--- src/kernel/locks.c
+++ src/kernel/locks.c
@@ -3,9 +3,15 @@
 // Functions for matched interrupt enabling ad disabling
 
 void lock_intr_enable(void) {
-    
+
 }
 
 void lock_intr_disable(void) {
+    int intr_state = get_intr();
+    disable_intr();
 
+    if (get_cpu_struct()->depth_lock_intr_disable == 0) {
+        get_cpu_struct->intr_prev_state = old;
+    }
+    get_cpu_struct()->depth_lock_intr_disable += 1;
 }
