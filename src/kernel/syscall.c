55eb966c1170f7e013f28db5a19fbcdae7f34c7d
JussiKarkkainen <juskark@gmail.com>
2022-03-03 21:06:46 +0200

    add syscall function array

--- src/kernel/syscall.c
+++ src/kernel/syscall.c
@@ -2,6 +2,32 @@
 #include "syscall.h"
 #include "process.h"
 
+
+uint32_t (*syscall[])(void) {
+    [SYS_FORK] sys_fork,
+    [SYS_EXIT] sys_exit,
+    [SYS_WAIT] sys_wait,
+    [SYS_PIPE] sys_pipe,
+    [SYS_READ] sys_read,
+    [SYS_KILL] sys_kill,
+    [SYS_EXEC] sys_exec,
+    [SYS_FSTAT] sys_fstat,
+    [SYS_CHDIR] sys_chdir,
+    [SYS_DUP] sys_dup,
+    [SYS_GETPID] sys_getpid,
+    [SYS_SBRK] sys_sbrk,
+    [SYS_SLEEP] sys_sleep,
+    [SYS_UPTIME] sys_uptime,
+    [SYS_OPEN] sys_open,
+    [SYS_WRITE] sys_write,
+    [SYS_MKNOD] sys_mknod,
+    [SYS_UNLINK] sys_unlink,
+    [SYS_LINK] sys_link,
+    [SYS_MKDIR] sys_mkdir,
+    [SYS_CLOSE] sys_close
+};
+
+
 // Handle syscalls, is called from utrap() in trap.c
 void syscall(void) {
     
