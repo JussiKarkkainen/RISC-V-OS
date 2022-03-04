8ea88c550f7fb1ab912ab7034e904c2848922873
JussiKarkkainen <juskark@gmail.com>
2022-03-04 20:49:56 +0200

    add context struct

--- src/kernel/process.h
+++ src/kernel/process.h
@@ -83,7 +83,23 @@ struct trapframe {
     uint32_t t6;
 };
 
+// This is used in scheduling between processes
 struct context {
+    uint32_t ra;
+    uint32_t sp;
+
+    uint32_t s0
+    uint32_t s1
+    uint32_t s2
+    uint32_t s3
+    uint32_t s4
+    uint32_t s5
+    uint32_t s6
+    uint32_t s7
+    uint32_t s8
+    uint32_t s9
+    uint32_t s10
+    uint32_t s11;
 };
 
 
