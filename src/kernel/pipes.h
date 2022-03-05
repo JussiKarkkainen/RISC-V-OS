da096648e42cd8c08fbd8a75bc7ebf9da2c3d08e
JussiKarkkainen <juskark@gmail.com>
2022-03-05 21:29:19 +0200

    add pipe struct

--- src/kernel/pipes.h
+++ src/kernel/pipes.h
@@ -1,9 +1,15 @@
 #ifndef PIPES_H
 #define PIPES_H
 
+#define PIPESIZE 512
+
 struct pipe {
-
+    struct spinlock lock;
+    char data[PIPESIZE];
+    unsigned int num_read;  // Number of bytes read
+    unsigned int num_write; // Number of bytes writte
+    int read_open;      
+    int write_open;
 };
-
 
 #endif
