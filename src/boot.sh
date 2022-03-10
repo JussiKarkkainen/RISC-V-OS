eeef1465db45fa94182aad689c20dac54f3f7c4d
JussiKarkkainen <juskark@gmail.com>
2022-03-10 20:39:42 +0200

    update compile and boot scripts

--- src/boot.sh
+++ src/boot.sh
@@ -1,6 +1,6 @@
 riscv64-unknown-elf-gcc -Wall -Wextra -march=rv32ima -mabi=ilp32 -T kernel/linker.ld -nostdlib -ffreestanding -lgcc \
 kernel/boot.S kernel/kernel.c kernel/console.c kernel/uart.c libc/stdio/kprintf.c kernel/pmm.c kernel/mem.S \
 libc/string/memset.c libc/stdio/panic.c kernel/paging.c libc/string/memmove.c libc/string/memcpy.c kernel/locks.c \
-kernel/process.c kernel/transfer.S kernel/mstart.c kernel/tvec.S
+kernel/process.c kernel/transfer.S kernel/mstart.c kernel/tvec.S kernel/trap.c
 
 qemu-system-riscv32 -nographic -smp 4 -m 128M -machine virt -bios none -kernel a.out
