e4ad30a7041c2e933f75f8bb434c03f91f6314b9
JussiKarkkainen <juskark@gmail.com>
2022-03-11 19:30:57 +0200

    update main boot sequence

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,20 +1,37 @@
 #include "uart.h"
 #include "../libc/include/stdio.h"
-//#include "paging.h"
+#include "paging.h"
+#include "process.h"
+#include "trap.h"
 
 // This should be executed in supervisor mode, boot.S should first call 
 // start() for setup before tranfering control to enter().
+static volatile int started = 0;
 
-void enter(void) {
-    kprintf("%s\n", "Booting OS");
+void enter(void) {  
+    
+    if (which_cpu() == 0) {
 
-    pmm_init();         // Initialize physical memory manager
-    kpage_init();       // Initilaize kernel pagetable
-    init_paging();      // Initialize paging
-    init_ktrapvec();    // Write ktrapvec addr into stvec to init trap handling
-    init_trapvec();     // Initialize timer trapvec
-    plic_init();        // Setup interrupt controller  
-    plic_init_hart();   // request device interrupts
-    console_init();     // Start console
-    scheduler();        // Start the scheduler
+        kprintf("%s\n", "Booting OS");
+
+        pmm_init();         // Initialize physical memory manager
+    //    test_alloc();
+        kpage_init();       // Initilaize kernel pagetable
+        init_paging();      // Initialize paging
+        init_ktrapvec();    // Write ktrapvec addr into stvec to init trap handling
+    //    init_trapvec();     // Initialize timer trapvec
+    //    plic_init();        // Setup interrupt controller  
+    //    plic_init_hart();   // request device interrupts
+        console_init();     // Start console
+    //    scheduler();        // Start the scheduler
+        __sync_synchronize();
+        started = 1;    
+    }
+    else {
+        while (started == 0) {
+            ;
+        }
+        __sync_synchronize();
+        kprintf("hart %d booting\n", which_cpu());
+    }
 }
