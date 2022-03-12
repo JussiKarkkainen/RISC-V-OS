e8354c99108d63a9acf22a3f82a26dfc47fb5e46
JussiKarkkainen <juskark@gmail.com>
2022-03-12 14:30:39 +0200

    set features

--- src/kernel/disk.c
+++ src/kernel/disk.c
@@ -38,7 +38,14 @@ void disk_init(void) {
     uint32_t guest_features = *(base_addr + DISK_GUEST_FEATURES);
     
     // 5. Negotiate the set of features and write what you'll accept to guest_features register
-        
+    uint32_t features = *(base_addr + DISK_HOST_FEATURES);
+    features &= ~(1 << VIRTIO_BLK_F_RO);
+    features &= ~(1 << VIRTIO_BLK_F_SCSI);
+    features &= ~(1 << VIRTIO_BLK_F_CONFIG_WCE);
+    features &= ~(1 << VIRTIO_BLK_F_MQ);
+    features &= ~(1 << VIRTIO_F_ANY_LAYOUT);
+    features &= ~(1 << VIRTIO_RING_F_EVENT_IDX);
+    features &= ~(1 << VIRTIO_RING_F_INDIRECT_DESC);
 
 }
 
