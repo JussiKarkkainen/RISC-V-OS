22857afa90b349d84281b113bac7f694541f7f71
JussiKarkkainen <juskark@gmail.com>
2022-03-14 21:05:02 +0200

    add buffer cache struct

--- src/kernel/bufcache.c
+++ src/kernel/bufcache.c
@@ -1,1 +1,26 @@
 // Buffer-Cache layer of the fileystem
+
+#include "disk.h"
+
+
+
+
+struct {
+    
+    struct spinlock lock;
+
+    // Create NUMBUF amount of buffers for buffer cache to hold
+    struct buffer[NUMBUF];
+    
+    // Buffers are in a linked list connected by buffer structs
+    // prev and next. Can be referred to with list_head.next/prev. 
+    struct buffer list_head;
+} buffer_cache;
+
+
+
+
+
+
+
+
