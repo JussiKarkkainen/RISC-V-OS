73322e455afe1a9d2b466c1057e369990851e4cf
JussiKarkkainen <juskark@gmail.com>
2022-03-14 21:17:49 +0200

    add function prototypes and documentation

--- src/kernel/bufcache.c
+++ src/kernel/bufcache.c
@@ -1,9 +1,13 @@
 // Buffer-Cache layer of the fileystem
+// The job of the buffer cache lyer is to synchronize access to disk blocks and
+// cache recently used buffers in a buffer cache. Main interface goes through 
+// buffer_read and buffer_write, the former reading a buffer from disk to memory
+// and the latter writing a modified buffer back to the disk.
+
 
 #include "disk.h"
-
-
-
+#include "locks.h"
+#include "process.h"
 
 struct {
     
@@ -19,8 +23,13 @@ struct {
 
 
 
+// Raad from disk
+struct buffer *buffer_read(void) {
+}
 
-
+// Write to disk
+void buffer_write(void) {
+}
 
 
 
