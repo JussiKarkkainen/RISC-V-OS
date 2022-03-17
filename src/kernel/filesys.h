1c4f82e16fe7a5272d657004d51f4a780b9f1dae
JussiKarkkainen <juskark@gmail.com>
2022-03-17 21:18:48 +0200

    add superblock struct

--- src/kernel/filesys.h
+++ src/kernel/filesys.h
@@ -15,6 +15,17 @@ struct buffer {
     char data[BUFFER_SIZE];
 };
 
+struct superblock {
+    unsigned int magic;
+    unsigned int size;          // Size of filesystem
+    unsigned int num_block;     // Number of data blocks
+    unsigned int num_log;       // log blocks
+    unsigned int log_start;     // Block num of first log block
+    unsigned int num_inodes; 
+    unsigned int inode_start;
+    unsigned int bitmap_start;
+}
+
 // Functions from bufcache.c
 void buffer_init(void);
 struct buffer *buffer_read(int dev, int blockno);
