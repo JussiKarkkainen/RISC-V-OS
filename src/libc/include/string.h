92ea7ec052573d928305b517b9ee430ccd231fab
JussiKarkkainen <juskark@gmail.com>
2022-03-22 21:45:06 +0200

    add strncmp

--- src/libc/include/string.h
+++ src/libc/include/string.h
@@ -8,5 +8,6 @@ void *memcpy(void *s1, const void *s2, size_t n);
 void *memset(void *str, int c, size_t n);
 size_t strlen(const char* str);
 void *memmove(void *dst, const void *src, size_t n);
+int strncmp(const char *a, const char *b, unsigned int n);
 
 #endif
---
+++ src/libc/string/strncmp.c
@@ -0,0 +1,15 @@
+#include "../include/string.h"
+
+int strncmp(const char *a, const char *b, unsigned int n) {
+    while (n && *a && (*a == *b)) {
+        n--;
+        a++;
+        b++;
+    }
+    if (n == 0) {
+        return 0;
+    }
+    else {
+        return (unsigned char)*a - (unsigned char)*b;
+    }
+}
