f5dcc965d9c64b40c8eb6827e83b659f7e9f672a
JussiKarkkainen <juskark@gmail.com>
2022-03-25 21:23:44 +0200

    add read and write syscalls

--- src/kernel/sysfile.c
+++ src/kernel/sysfile.c
@@ -1,9 +1,9 @@
 #include "sysfile.h"
 #include "filesys.h"
+#include "file.h"
 #include <stdint.h>
 
 // File system system calls
-
 
 uint32_t sys_exec(void) {
     
@@ -13,10 +13,28 @@ uint32_t sys_exec(void) {
 
 
 uint32_t sys_read(void) {
+
+    struct file *file;
+    int n;
+    uint32_t p;
+
+    if (argfd(0, 0, &file) < 0 || argint(2, &n) < 0 || argaddr(1, &p) < 0) {
+        return -1;
     }
+    return file;
+}
 
 
 uint32_t sys_write(void) {
+    
+    struct file *file;
+    int n;
+    uint32_t p;
+
+    if (argfd(0, 0, &file) < 0 || argint(2, &n) < 0 || argaddr(1, &p) < 0) {
+        return -1;
+    }
+    return write_file(file, p, n);
 }
 
 
