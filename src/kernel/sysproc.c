caa09d59b48ba77a0ee05a185504a271969cd688
JussiKarkkainen <juskark@gmail.com>
2022-03-28 19:01:20 +0300

    add process syscalls

--- src/kernel/sysproc.c
+++ src/kernel/sysproc.c
@@ -1,4 +1,6 @@
 #include "syscall.h"
+#include "process.h"
+#include "locks.h"
 #include <stdint.h>
 
 
@@ -12,37 +14,74 @@ uint32_t sys_exit(void) {
     return 0;
 }
 
-
-
 uint32_t sys_getpid(void) {
-}
 
+    return get_process_struct();
+}
 
 uint32_t sys_fork(void) {
-}
 
+    return fork();
+}
 
 uint32_t sys_wait(void) {
-}
 
+    uint32_t i;
+    if (argaddr(0, &i) < 0) {
+        return -1;
+    }
+    return wait(p);
+}
 
 uint32_t sys_sbrk(void) {
-}
 
+    int addr, n;
+    if (argaddr(0, &n) < 0) {
+        return -1;
+    }
+    addr = get_process_struct()->size;
+    if (growproc(n) < 0) {
+        return -1;
+    }
+    return addr;
+}
 
 uint32_t sys_sleep(void) {
-}
+   
+   int n;
+   unsigned int ticks0;
 
+   if (argint(0, &n) < 0) {
+       return -1;
+    }
+    acquire_lock(&ticklock);
+    ticks0 = ticks;
+    while(ticks - ticks0 < n) {
+        if (get_process_struct()->killed) {
+            release_lock(&tickslock);
+            return -1;
+        }
+        sleep(&tickslock);
+    }
+    release_lock(&tickslock);
+    return 0;
+}
 
 uint32_t sys_kill(void) {
+    int pid;
+    
+    if (argint(0, &pid) < 0) {
+        return -1;
+    }
+    return kill(pid);
 }
-
 
 uint32_t sys_uptime(void) {
+
+    unsigned int xticks;
+
+    acquire_lock(&tickslock);
+    xticks = ticks;
+    release_lock(&tickslock);
+    return xticks;
 }
-
-
-
-
-
-
