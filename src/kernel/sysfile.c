43b06d60174598768a0708e280dcc586f30024ac
JussiKarkkainen <juskark@gmail.com>
2022-03-29 21:45:21 +0300

    add exec function

--- src/kernel/sysfile.c
+++ src/kernel/sysfile.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include "syscall.h"
+#include "paging.h"
 
 // File system system calls
 
@@ -40,6 +41,170 @@ int fdalloc(struct file *f) {
         }
     } 
     return -1;
+}
+
+int exec(char *path, char **argv) {
+    char *s, *last;
+    int i, off;
+    uint32_t argc, sz = 0, sp, ustack[MAXARG], stackbase;       // These need to be added
+    struct elfhdr elf;
+    struct inode *ip;
+    struct proghdr ph;
+    uint32_t *pagetable = 0, oldpagetable;
+    struct proc *p = get_rocess_struct();
+
+    begin_op();
+
+    if ((ip = name_inode(path)) == 0) {
+        end_op();
+        return -1;
+    }
+    inode_lock(ip);
+
+    // Check ELF header
+    if (read_inode(ip, 0, (uint32_t)&elf, 0, sizeof(elf)) != sizeof(elf)) {
+        goto bad;
+    }
+    if (elf.magic != ELF_MAGIC) {
+        goto bad;
+    }
+
+    if ((pagetable = proc_pagetable(p)) == 0) {
+        goto bad;
+    }
+
+    // Load program into memory.
+    for (i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)) {
+        if (read_inode(ip, 0, (uint32_t)&ph, off, sizeof(ph)) != sizeof(ph)) {
+            goto bad;
+        }
+        if (ph.type != ELF_PROG_LOAD) {
+            continue;
+        }
+        if (ph.memsz < ph.filesz) {
+            goto bad;
+        }
+        if (ph.vaddr + ph.memsz < ph.vaddr) {
+            goto bad;
+        }
+        
+        uint32_t sz1;
+        
+        if ((sz1 = uvmalloc(pagetable, sz, ph.vaddr + ph.memsz)) == 0) {
+            goto bad;
+        }
+        sz = sz1;
+        if ((ph.vaddr % PGESIZE) != 0) {
+            goto bad;
+        }
+        if (loadseg(pagetable, ph.vaddr, ip, ph.off, ph.filesz) < 0) {
+            goto bad;
+        }
+    }
+    
+    inode_unlock(ip);
+    inode_put(ip);
+    end_op();
+    ip = 0;
+
+    p = get_process_struct();
+    uint32_t oldsz = proc->size;
+
+    // Allocate two pages at the next page boundary.
+    // Use the second as the user stack.
+    sz = PGROUNDUP(sz);
+    uint32_t sz1;
+    if ((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE)) == 0) {
+        goto bad;
+    }
+
+    sz = sz1;
+    uvmclear(pagetable, sz-2*PGSIZE);
+    sp = sz;
+    stackbase = sp - PGSIZE;
+
+    // Push argument strings, prepare rest of stack in ustack.
+    for (argc = 0; argv[argc]; argc++) {
+        if(argc >= MAXARG) {
+            goto bad;
+        }
+        sp -= strlen(argv[argc]) + 1;
+        sp -= sp % 16; // riscv sp must be 16-byte aligned
+        if (sp < stackbase) {
+            goto bad;
+        }
+        if (copyout(pagetable, sp, argv[argc], strlen(argv[argc]) + 1) < 0) {
+            goto bad;
+        }
+        ustack[argc] = sp;
+    }
+    ustack[argc] = 0;
+
+    // push the array of argv[] pointers.
+    sp -= (argc+1) * sizeof(uint32_t);
+    sp -= sp % 16;
+    if (sp < stackbase) {
+        goto bad;
+    }
+    if (copyout(pagetable, sp, (char *)ustack, (argc+1)*sizeof(uint32_t)) < 0) {
+        goto bad;
+    }
+
+    // arguments to user main(argc, argv)
+    // argc is returned via the system call return
+    // value, which goes in a0.
+    p->trapframe->a1 = sp;
+
+    // Save program name for debugging.
+    for (last=s=path; *s; s++) {
+        if(*s == '/') {
+            last = s+1;
+        }
+    }
+    strcpy(p->name, last, sizeof(p->name));
+
+    // Commit to the user image.
+    oldpagetable = p->pagetable;
+    p->pagetable = pagetable;
+    p->size = sz;
+    p->trapframe->saved_pc = elf.entry;  // initial program counter = main
+    p->trapframe->sp = sp; // initial stack pointer
+    proc_freepagetable(oldpagetable, oldsz);
+
+    return argc; // this ends up in a0, the first argument to main(argc, argv)
+
+    bad:
+        if (pagetable) {
+            proc_freepagetable(pagetable, sz);
+        }
+    if (ip) {
+        inode_unlock(ip);
+        inode_put(ip);
+        end_op();
+    }
+    return -1;
+}
+
+int loadseg(pagetable_t pagetable, uint64 va, struct inode *ip, uint offset, uint sz) {
+    unsigned int i, n;
+    uint32_t pa;
+
+    for (i = 0; i < sz; i += PGESIZE) {
+        pa = walk(pagetable, va + i);
+        if (pa == 0) {
+            panic("loadseg: address should exist");
+        }
+        if (sz - i < PGESIZE) {
+            n = sz - i;
+        }
+        else {
+            n = PGESIZE;
+        }
+        if (read_inode(ip, 0, (uint32_t)pa, offset+i, n) != n) {
+            return -1;
+        }
+    }
+    return 0;
 }
 
 uint32_t sys_exec(void) {
