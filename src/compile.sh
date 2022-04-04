cf2bddf81018ae7434ddc0f570ce9622d9e59d96
JussiKarkkainen <juskark@gmail.com>
2022-04-04 15:03:07 +0300

    update compile script

--- src/compile.sh
+++ src/compile.sh
@@ -2,6 +2,4 @@ riscv64-unknown-elf-gcc -Wall -Wextra -mcmodel=medany -march=rv32ima -mabi=ilp32
 kernel/boot.S kernel/kernel.c kernel/console.c kernel/uart.c libc/stdio/kprintf.c kernel/pmm.c kernel/mem.S \
 libc/string/memset.c libc/stdio/panic.c kernel/paging.c libc/string/memmove.c libc/string/memcpy.c kernel/locks.c \
 kernel/process.c kernel/transfer.S kernel/mstart.c kernel/tvec.S kernel/trap.c kernel/plic.c kernel/ktrapvec.S \
-kernel/syscall.c kernel/filesys.c 
-
-#kernel/file.c kernel/disk.c kernel/bufcache.c 
+kernel/syscall.c kernel/filesys.c kernel/sysfile.c kernel/sysproc.c kernel/file.c kernel/bufcache.c kernel/disk.c
