a7673aa0bbd008b77f36dbe6de69c5a1cf132636
JussiKarkkainen <juskark@gmail.com>
2022-04-11 21:45:22 +0300

    add defs to user.h

--- src/user/user.h
+++ src/user/user.h
@@ -1,4 +1,10 @@
 
+#define O_RDONLY  0x000
+#define O_WRONLY  0x001
+#define O_RDWR    0x002
+#define O_CREATE  0x200
+#define O_TRUNC   0x400
+
 // system calls
 int fork(void);
 int exit(int) __attribute__((noreturn));
