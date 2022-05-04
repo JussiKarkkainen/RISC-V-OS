fb8235c2fe68e9650c0e92e8b6829e56bc5fb95b
JussiKarkkainen <juskark@gmail.com>
2022-05-04 19:31:08 +0300

    add struct to describe pci config space

--- src/net/virtio-net.c
+++ src/net/virtio-net.c
@@ -4,7 +4,34 @@
 #include "../libc/include/stdio.h"
 #include <stdint.h>
 
-#define R(r) ((volatile uint32_t *)(VIRT_PCIE + (r))) 
+#define VIRT32_HIGH_PCIE_MMIO_BASE  0x30000000ULL
+
+#define R(r) ((volatile uint32_t *)(VIRT32_HIGH_PCIE_MMIO_BASE + (r))) 
+
+#define PCIE_ADDR 0x000
+#define PCIE_SUBSYSTEM_ID 0x2c
+
+uint32_t *phy_addr = (VIRT32_HIGH_PCIE_MMIO_BASE + (0x00 << 20 | 0x01 << 15 | 0x0 << 12));
+
+// https://wiki.osdev.org/PCI#The_PCI_Bus
+struct pci_config {
+    uint32_t device_id;
+    uint32_t status;
+    uint32_t class_code;
+    uint32_t BIST;
+    uint32_t BAR0;
+    uint32_t BAR1;
+    uint32_t BAR2;
+    uint32_t BAR3;
+    uint32_t BAR4;
+    uint32_t BAR5;
+    uint32_t cardbus;
+    uint32_t susbystem_id;
+    uint32_t ROM_base_addr;
+    uint32_t reserved;
+    uint32_t reserved:
+    uint32_t latency;
+}
 
 struct Virtio_net {
         
@@ -14,13 +41,19 @@ struct Virtio_net {
 
 
 void virtio_net_init(void) {
-    
-    initlock(&net.net_lock, "net_lock");
    
-    // 1. Reset device
-    uint32_t *val = *R(0);
-    kprintf("val: %p\n", *val);
+    initlock(&net.net_lock, "net_lock");
+    kprintf("phy_addr %p\n", *phy_addr); 
+    kprintf("mcfg %p\n", *R(PCIE_SUBSYSTEM_ID));
 
 
 
+}
+
+
+int virtio_net_send() {
+}
+
+
+int virtio_net_recv() {
 }
--- src/net/virtio-net.h
+++ src/net/virtio-net.h
@@ -1,10 +1,12 @@
 #ifndef NET_H
 #define NET_H
 
-#define VIRT_PCIE 0x40000000
+#define VIRT_PCIE 0x30000000
 
 
 void virtio_net_init(void);
 
+int virtio_net_send();
+int virtio_net_recv();
 
 #endif
