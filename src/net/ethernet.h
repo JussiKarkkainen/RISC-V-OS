d4cfb64f3347538760b8a5681b7d53aad5a6e443
JussiKarkkainen <juskark@gmail.com>
2022-05-07 12:55:06 +0300

    add ethernet header struct

--- src/net/ethernet.h
+++ src/net/ethernet.h
@@ -1,6 +1,16 @@
 #ifndef ETHERNET_H
 #define ETHERNET_H
 
+#include <stdint.h>
+
+struct ethernet_hdr {
+    unsigned char dst_mac[6];
+    unsigned char src_mac[6];
+    uint16_t ethertype;
+    unsigned char payload[];
+}__attribute__((packed));
+
+
 void ethernet_send_frame();
 void ethernet_receive_frame();
 
