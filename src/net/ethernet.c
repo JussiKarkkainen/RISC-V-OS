748801fa323e7bdc8747a25bccb18dc91d007ca7
JussiKarkkainen <juskark@gmail.com>
2022-05-07 14:10:07 +0300

    start ethernet send and recv functions

--- src/net/ethernet.c
+++ src/net/ethernet.c
@@ -1,12 +1,35 @@
 #include "ethernet.h"
 #include <stdint.h>
+#include <stddef.h>
+#include <arpa/inet.h>
 
+void ethernet_send_frame(uint8_t dst_mac_addr, uint8_t *data, uint32_t len, uint16_t protocol) {
+    uint8_t src_mac_addr[6];
 
-void ethernet_send_frame() {
+    struct ethernet_hdr eth_hdr = kalloc();
+
+    virtio_net_send(frame, sizeof(ethernet_hdr) + len);
 }
 
 
-void ethernet_receive_frame() {
+void ethernet_receive_frame(uint8_t *data, uint32_t len) {
+
+    struct ethernet_hdr eth_hdr;
+
+    switch (eth_hdr.ethertype) {
+        case ETHERTYPE_ARP:
+            arp_receive_packet();
+            break;
+        
+        case ETHERTYPE IPV4:
+            ipv4_receive_packet();
+            break;
+
+        default:
+            kprintf("Unsupported ethernet frame type=%p\n", eth_hdr.ethertype);
+    }
+
+    kfree(data);
 }
 
 
