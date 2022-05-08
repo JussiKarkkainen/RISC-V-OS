0f8379e3cc3fe8ce7d2062749e2e0395516b963f
JussiKarkkainen <juskark@gmail.com>
2022-05-08 13:48:55 +0300

    continue ethernet receive frame

--- src/net/ethernet.c
+++ src/net/ethernet.c
@@ -1,7 +1,10 @@
 #include "ethernet.h"
+#include "../libc/include/string.h"
+#include "../libc/include/stdio.h"
 #include <stdint.h>
 #include <stddef.h>
 #include <arpa/inet.h>
+
 
 void ethernet_send_frame(uint8_t dst_mac_addr, uint8_t *data, uint32_t len, uint16_t protocol) {
     uint8_t src_mac_addr[6];
@@ -15,6 +18,8 @@ void ethernet_send_frame(uint8_t dst_mac_addr, uint8_t *data, uint32_t len, uint
 void ethernet_receive_frame(uint8_t *data, uint32_t len) {
 
     struct ethernet_hdr eth_hdr;
+    memcpy(&eth_hdr, data, len);
+    eth_hdr.ethertype = ntohs(eth_hdr.ethertype);
 
     switch (eth_hdr.ethertype) {
         case ETHERTYPE_ARP:
