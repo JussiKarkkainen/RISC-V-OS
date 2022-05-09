29137b5bf0e632d7b9f32d0b8256fe22ed8fdca9
JussiKarkkainen <juskark@gmail.com>
2022-05-09 21:59:24 +0300

    add files for arp protocol

---
+++ src/net/arp.c
@@ -0,0 +1,14 @@
+#include "arp.h"
+
+
+
+void arp_receive_packet() {
+}
+
+
+void arp_request_mac_addr() {
+}
+
+
+void arp_reply() {
+}
---
+++ src/net/arp.h
@@ -0,0 +1,9 @@
+#ifndef ARP_H
+#define ARP_H
+
+
+
+
+
+
+#endif
