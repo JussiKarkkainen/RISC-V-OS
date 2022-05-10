175f7d2309d9a048001f610084221c2c2eadc811
JussiKarkkainen <juskark@gmail.com>
2022-05-10 21:58:34 +0300

    start virtio_send and fix net header struct

--- src/net/virtio-net.c
+++ src/net/virtio-net.c
@@ -20,39 +20,51 @@ void virtio_net_init(void) {
     initlock(&net.net_lock, "net_lock");
     pcie_init();
      
-    struct virtio_net_device *net_device = (struct virtio_net_device *)PCIE_MMIO_BASE;
+    struct virtio_pci_common_cfg *net_common_cfg = (struct virtio_net_device *)PCIE_MMIO_BASE;
     
-    uint32_t magic_val = (uint32_t)*R(DISK_MAGIC_VAL);
-//    kprintf("heelo\n", net_device->common_cfg->num_queues);
-    kprintf("magic val %p\n", magic_val);
 
-    uint16_t *num_queues = (uint16_t)net_device->common_cfg.num_queues;
-    kprintf("net_device->num_queues %p\n", num_queues);
-/*
     // Device initialization described in virtio specification
-    net_device->common_cfg->device_status = VIRTIO_DEV_RESET;
-    net_device->common_cfg->device_status = VIRTIO_DEV_STATUS_ACKNOWLEDGE;
-    net_device->common_cfg->device_status |= VIRTIO_DEV_STATUS_DRIVER;
+    net_common_cfg->device_status = VIRTIO_DEV_RESET;
+    net_common_cfg->device_status = VIRTIO_DEV_STATUS_ACKNOWLEDGE;
+    net_common_cfg->device_status |= VIRTIO_DEV_STATUS_DRIVER;
     
-    uint32_t features = net_device->common_cfg->device_status;
-    features &= ~(1 << VIRTIO_NET_CSUM);
-    net_device->common_cfg->device_status = features;
+    uint32_t features = net_common_cfg->device_status;
+    features &= ~(1 << VIRTIO_NET_F_CSUM);
+    net_common_cfg->device_status = features;
     
-    net_device->common_cfg->device_status |= VIRTIO_DEV_STATUS_FEATURES_OK;
+    net_common_cfg->device_status |= VIRTIO_DEV_STATUS_FEATURES_OK;
+    
+    uint32_t device_status = net_common_cfg->device_status;
+    kprintf("device_status %p\n", device_status);
 
     // Setupt queue 0 (receiveq1) and 1 (transmitq1)
-    net_device->common_cfg->queue_select = 0;
-    uint16_t queue_size = net_device->common_cfg->queue_size;
+    net_common_cfg->queue_select = 0;
+    uint16_t queue_size = net_common_cfg->queue_size;
+    kprintf("queue_size %p\n", queue_size);
     
-*/
-
-    
-
+     
 
 }
 
 
-int virtio_net_send() {
+int virtio_net_send(int payload_size) {
+    
+    int size = size + sizeof(virtio_net_hdr);
+
+    if (size > 1526) {
+        kprintf("packet size is too big %p\n", size);
+        return 0;
+    }
+    
+    virtio_net_header net_hdr;
+    net_hdr.flags = VIRTIO_NET_HDR_F_NEEDS_CSUM;
+    net_hdr.gso_type = 0;
+    net_header.csum_start = 0;
+    net_header.csum_offset = size;
+
+
+
+
 }
 
 
--- src/net/virtio-net.h
+++ src/net/virtio-net.h
@@ -69,18 +69,18 @@ struct virtio_net_hdr {
     #define VIRTIO_NET_HDR_F_NEEDS_CSUM 1
     #define VIRTIO_NET_HDR_F_DATA_VALID 2
     #define VIRTIO_NET_HDR_F_RSC_INFO 4
-    u8 flags;
+    uint8_t flags;
     #define VIRTIO_NET_HDR_GSO_NONE 0
     #define VIRTIO_NET_HDR_GSO_TCPV4 1
     #define VIRTIO_NET_HDR_GSO_UDP 3
     #define VIRTIO_NET_HDR_GSO_TCPV6 4
     #define VIRTIO_NET_HDR_GSO_ECN 0x80
-    u8 gso_type;
-    le16 hdr_len;
-    le16 gso_size;
-    le16 csum_start;
-    le16 csum_offset;
-    le16 num_buffers;
+    uint8_t gso_type;
+    uint16_t hdr_len;
+    uint16_t gso_size;
+    uint16_t csum_start;
+    uint16_t csum_offset;
+    uint16_t num_buffers;
 };
 
 void virtio_net_init(void);
