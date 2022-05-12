a8a0667e937d9dddebbf7664928903c0d5f44b60
JussiKarkkainen <juskark@gmail.com>
2022-05-12 21:58:22 +0300

    add arp packet struct

--- src/net/arp.h
+++ src/net/arp.h
@@ -1,9 +1,19 @@
 #ifndef ARP_H
 #define ARP_H
 
+#include "stdint.h"
 
-
-
+struct arp_packet {
+    uint16_t hardware_type;
+    uint16_t protocol_type;
+    uint8_t hardware_size;
+    uint8_t protocol_size;
+    uint16_t opcode;
+    uint8_t src_mac[6];
+    uint8_t src_ip[4];
+    uint8_t dst_mac[6];
+    uint8_t dst_ip[4];
+} __attribute__((packed));
 
 
 #endif
