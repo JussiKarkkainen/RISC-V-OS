bff5bdfd394e8eb5a6d9143285e477eaa06f954d
JussiKarkkainen <juskark@gmail.com>
2022-06-03 21:37:55 +0300

    start arp protocol

--- src/net/arp.c
+++ src/net/arp.c
@@ -2,7 +2,25 @@
 
 
 
-void arp_receive_packet() {
+void arp_receive_packet(uint8_t *data, uint32_t len) {
+    struct arp_packet arp_packet;
+    memcpy(&arp_packet, data, sizeof(struct arp_packet);
+    arp_packet.hardware_type = ntohs(arp_packet.hardware_type);
+    arp_protocol_type = ntohs(arp_packet.protocol_type);
+    arp_packet.opcode = ntohs(arp_packet.opcode);
+
+
+    switch (arp_packet.opcode) {
+        case ARP_REQUEST:
+            break;
+
+        case ARP_REPLY:
+            break;
+
+        default:
+            kprintf("unrecognized arp opcode\n");
+            break;
+    }
 }
 
 
