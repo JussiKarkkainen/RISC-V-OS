3ccde905d5a8fa9ad30fbf15ae4513985f2c1360
JussiKarkkainen <juskark@gmail.com>
2022-06-09 15:53:34 +0300

    update tco header

--- src/net/tcp.c
+++ src/net/tcp.c
@@ -1,9 +1,21 @@
 #include "tcp.h"
+#include "ipv4.h"
 #include <stdint.h>
+#include <stddef.h>
+
+
+
+
+void tcp_connect() {
+}
+
+
+
 
 
 void tcp_receive_packet(struct *ipv4hdr, uint8_t *data) {
 }
 
 
-
+void tcp_send_packet() {
+}
--- src/net/tcp.h
+++ src/net/tcp.h
@@ -8,12 +8,13 @@ struct tcp_header {
     uint16_t dst_port;
     uint32_t sequence_num;
     uint32_t acknowledge_num;
-    uint8_t data_offset;
+    uint8_t header_length : 4;
+    uint8_t resv : 4;
     uint8_t flags;
     uint16_t window_size;
     uint16_t tcp_checksum;
     uint16_t urgent_pointer;
-
+    uint32_t options[10];
 } __attribute__((packed));
 
 #endif
