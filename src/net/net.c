e44a10d095849c693a04e842184ace74deb76f5f
JussiKarkkainen <juskark@gmail.com>
2022-06-10 15:56:07 +0300

    add net structs and function defs

--- src/net/net.c
+++ src/net/net.c
@@ -3,6 +3,12 @@
 #include <stdint.h>
 
 
+int getaddrinfo(const char *restrict node,
+                const char *restrict service,
+                const struct addrinfo *restrict hints,
+                struct addrinfo **restrict res) {
+}
+
 int socket(int domain, int type, int protocol) {
 }
 
@@ -17,4 +23,8 @@ int recvfrom(int sockfd, void *buf, size_t len, int flags,
                  struct sockaddr *src_addr, socklen_t *addrlen) {
 }
 
+int send(int socket, const void *buffer, size_t length, int flags) {
+}
 
+int recv(int socket, void *buffer, size_t length, int flags) {
+}
