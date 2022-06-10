15f362eff9613c92ed8ad0a5136ffd270eabf6fb
JussiKarkkainen <juskark@gmail.com>
2022-06-10 10:56:35 +0300

    works on linux, now make it work on own os

--- src/user/getwebsite.c
+++ src/user/getwebsite.c
@@ -6,61 +6,48 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
 
-#define MAXLINE 100 
-#define MAXSUB 100
+int main() {
 
-int main(int argc, char *argv[]) {
 
-    if (argc < 2) {
-        printf("Usage: getwebsite domain\n");
-        return -1;
-    }
-
-    char *web_domain = argv[1];
-    char *page = "/";
-
-    char sendline[MAXLINE + 1], recvline[MAXLINE + 1];
-
-    // Form request
-    snprintf(sendline, MAXSUB,
-     "GET %s HTTP/1.0\r\n"
-     "Host: %s\r\n", page, web_domain);
-
+    char buf[2056];
+    
+    char *header = "GET / HTTP/1.1\r\nHost: info.cern.ch\r\n\r\nConnection: close\r\n\r\n";
 
     struct addrinfo hints, *res; 
     memset(&hints, 0, sizeof(hints));
 
-    hints.ai_family = AF_INET;
+    hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    printf("web_domain %s\n", web_domain);
-    getaddrinfo(web_domain, "80", &hints, &res);
+    getaddrinfo("info.cern.ch", "80", &hints, &res);
     
-    int sockfd;
+    int sockfd, cnt;
     
-    if (sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol) < 0) {
+    if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
+   
+        printf("sockfd = %d\n", sockfd); 
         printf("socket() returned error\n");
         return -1;
     }
-    printf("sockfd = %d\n", sockfd); 
-    int cnt;
-     
-    if (cnt = connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
-        printf("count %d\n", cnt);
-        printf("connect() returned error %d\n", cnt); 
-        return -1;
-    }
     
+    if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
+        printf("connect returned error");
+        return -1;
+    }  
+
     size_t n;
 
     // Send request
-    if (send(sockfd, sendline, strlen(sendline), 0) >= 0) {
+    send(sockfd, header, strlen(header), 0);
         // read response
-        while (n = recv(sockfd, recvline, MAXLINE, 0) > 0) {
-            recvline[n] = '\0';
-
-            printf("hello %s\n", recvline);
+    while (1) {
+        n = recv(sockfd, buf, sizeof(buf)-1, 0);
+        if (n == 0) {
+            break;
         }
+        buf[n] = 0;
+        printf("%s\n", buf);
     }
 
     return 0;
