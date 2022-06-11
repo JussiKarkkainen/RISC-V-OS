067c17933caf57e8b75e72f4daa2ec8767167c9d
JussiKarkkainen <juskark@gmail.com>
2022-06-11 09:42:45 +0300

    add socket alloc

--- src/net/socket.c
+++ src/net/socket.c
@@ -1,14 +1,44 @@
 #include "socket.h"
+#include "../kernel/paging.h"
+#include "../kernel/file.h"
 #include <stdint.h>
-
-
+#include <stddef.h>
 
 
 struct *file socket_alloc(int domain, int type, int protocol) {
+
+    struct file *file;
+    struct socket *socket;
+
+    if (domain != AF_INET || (type != SOCK_DGRAM && type != SOCK_STREAM) || protocol != 0) {
+        return NULL;
+    }
+
+    if (!(file = file_alloc())) {
+        return NULL;
+    }
+    
+    socket = (struct socket *)kalloc();
+    if (!s) {
+        return NULL;
+    }
+
+    socket->type = type;
+    socket->desc = (type == SOCK_STREAM ? tcp_open() : udp_open);
+    file->type = FD_SOCKET;
+    file->readable = 1;
+    file->writable = 1;
+    file->socket = socket;
+    return file;
 }
 
 
 int socket_connect(struct socket *s, struct sockaddr *addr, int addrlen) {
+
+    if (s->type != SOCK_STREAM) {
+        return -1;
+    }
+    return tcp_connect(s->desc, addr, addrlen);
 }
 
 
@@ -46,9 +76,3 @@ int socket_recvfrom(struct socket *s, char *buf, int n, struct sockaddr *addr, i
     }
     return udp_recvfrom(s->desc, (uint8_t *)buf, n, addr, addrlen);
 }
-
-
-
-
-
-
