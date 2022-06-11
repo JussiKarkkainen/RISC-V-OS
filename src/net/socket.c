b38d1a07480573b3d775820d8cf909d9a8148c59
JussiKarkkainen <juskark@gmail.com>
2022-06-11 09:24:27 +0300

    add socket send and receive

--- src/net/socket.c
+++ src/net/socket.c
@@ -1,5 +1,5 @@
 #include "socket.h"
-
+#include <stdint.h>
 
 
 
@@ -13,10 +13,20 @@ int socket_connect(struct socket *s, struct sockaddr *addr, int addrlen) {
 
 
 int socket_read(struct socket *s, char *addr, int n) {
+
+    if (s->type != SOCK_STREAM) {
+        return -1;
+    }
+    return tcp_recv(s->desc, (uint8_t *)addr, n);
 }
 
 
 int socket_write(struct socket *s, char *addr, int n) {
+
+    if (s->type != SOCK_STREAM) {
+        return -1;
+    }
+    return tcp_send(s->desc, (uint8_t *)addr, n);
 }
 
 
