b08ac9ae4d2877f8d6d8f72824edfd57a92db23a
JussiKarkkainen <juskark@gmail.com>
2022-06-12 21:37:31 +0300

    add sysrecvfrom

--- src/net/sysnet.c
+++ src/net/sysnet.c
@@ -21,6 +21,23 @@ int sys_sendto(void) {
 }
 
 int sys_recvfrom(void) {
+
+    struct file *file;
+    int n;
+    char *ptr;
+    int *addrlen;
+    struct sockaddr *addr;
+
+    if (argfd(0, 0, &file) < 0 || argint(2, &n) < 0 || argptr(1, &ptr, n) < 0 || argptr(4, (void *)&addrlen, sizeof(*addrlen)) < 0) {
+        return -1;
+    }
+    if (addrlen && argptr(3, (void*)&addr, *addrlen) < 0) {
+        return -1;
+    }
+    if (file->type != FD_SOCKET) {
+        return -1;
+    }
+    return socket_recvfrom(file->socket, addr, n, addr, addrlen);
 }
 
 int sys_send(void) {
