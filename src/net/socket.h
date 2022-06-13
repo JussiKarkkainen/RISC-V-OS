a648594245aa6ac7beb1e0f0c7c0ab44dee8fa7f
JussiKarkkainen <juskark@gmail.com>
2022-06-13 19:20:36 +0300

    move socket structs to socket.h

--- src/net/socket.h
+++ src/net/socket.h
@@ -3,6 +3,38 @@
 
 #include "../kernel/file.h"
 
+typedef socklen_t uint32_t;
+
+struct sockaddr {
+    unsigned short sa_family;
+    char sa_data[14];
+};
+
+struct in_addr {
+    unsigned long s_addr;
+};
+
+struct sockaddr_in {
+    short int sin_family;
+    unsigned short int sin_port;
+    struct in_addr sin_addr;
+    unsigned char sin_zero[8];
+};
+
+
+struct addrinfo {
+    int ai_flags;
+    int ai_family;
+    int ai_socket;
+    int ai_protocol;
+    size_t ai_addrlen;
+    struct sockaddr *ai_addr;
+    char *ai_canonname;
+
+    struct addrinfo *ai_next;
+};
+
+
 struct socket {
     int type;
     int desc;
