ac8341c6f152ada676a5d238c19aa6bbca1f4e2b
JussiKarkkainen <juskark@gmail.com>
2022-06-14 15:05:54 +0300

    add send function

--- src/net/tcp.c
+++ src/net/tcp.c
@@ -9,6 +9,20 @@
 // while tcp_send, and tcp_recv are used as an interface between
 // socket syscalls and tcp protocol
 
+#define TCP_CB_STATE_ESTABLISHED 4
+#define TCP_CB_STATE_CLOSE_WAIT  9
+
+#define TCP_CB_TABLE_SIZE 16
+#define SOCKET_INVALID(x) (x < 0 || x >= TCP_CB_TABLE_SIZE)
+#define TCP_CB_STATE_TX_ISREADY(x) (x->state == TCP_CB_STATE_ESTABLISHED || x->state == TCP_CB_STATE_CLOSE_WAIT)
+
+#define TCP_FLG_FIN 0x01
+#define TCP_FLG_SYN 0x02
+#define TCP_FLG_RST 0x04
+#define TCP_FLG_PSH 0x08
+#define TCP_FLG_ACK 0x10
+#define TCP_FLG_URG 0x20
+
 static struct spinlock tcplock;
 
 
@@ -25,9 +39,26 @@ void tcp_open(void) {
 }
 
 void tcp_send(int desc, uint8_t *buf, int len) {
+    
+    struct tcp_cb *cb;
 
-
-    tcp_send_packet();
+    if (SOCKET_INVALID(desc)) {
+        return -1;
+    }
+    acquire_lock(&tcplock);
+    cb = &cb_table[desc];
+    if (!cb->used) {
+        release_lock(&tcp_lock);
+        return -1;
+    }
+    if (!CB_STATE_IS_READY(cb)) {
+        release_lock(&tcp_lock);
+        return -1;
+    }
+    tcp_send_packet(cb, cb->send.next, cb->receive.next, TCP_FLG_ACK | TCP_FLG_PSH, buf, len);
+    cb->send.next += len;
+    release_lock(&tcplock);
+    return 0;
 }
 
 
