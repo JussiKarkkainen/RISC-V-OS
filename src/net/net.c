9e0751679e4869855d6edbaf27977ee3a365924e
JussiKarkkainen <juskark@gmail.com>
2022-06-15 11:51:56 +0300

    update net interface

--- src/net/net.c
+++ src/net/net.c
@@ -1,7 +1,18 @@
 #include "net.h"
 
+#define MAX_NET_INTERFACE 1     // For now...
 
+static struct net_interface net_interface[MAX_NET_INTERFACE];
 
+void net_interface_init(void) {
+           
+    struct net_interface *netif = kalloc(); 
+    netif->id = 0;
+    memcpy(netif->mac, get_driver_mac_addr(), 6);
+    memcpy(netif->ip, ip, 6);
+    memcpy(netif->gateway_ip, gateway_ip, 6);
+
+    net_interface[id] = netif; 
 
 
 
--- src/net/net.h
+++ src/net/net.h
@@ -3,24 +3,11 @@
 
 
 struct net_interface {
-    struct net_interface *next;
-    uint8_t family;
-    struct net_dev *dev;
-};
-
-struct net_dev {
-    struct net_dev *next;
-    struct net_interface *ifs; 
-    int index;
-    uint16_t type;
-    uint16_t mtu;
-    uint16_t flags;
-    uint16_t hlen;
-    uint16_t alen;
-    uint8_t addr[16];
-    uint8_t peer[16];
-    uint8_t broadcast[16];
-    void *priv;
+    uint8_t id;
+    uint8_t mac[6];
+    uint8_t ip[4];
+    uint8_t gateway_mac[6];
+    uint8_t gateway_ip[4];
 };
 
 
